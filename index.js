80e9c926d03ed0a0544a7323aa83f13357014786
JN <joyng808@gmail.com>
2025-11-17 23:07:00 +0800

    [JN]-Update

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//-------------------------------------//
+// 75.Creating First Class and Instance
+//-------------------------------------//
 var User = /** @class */ (function () {
     function User() {
         this.name = "Jhon";
@@ -9,4 +12,20 @@ var User = /** @class */ (function () {
     return User;
 }());
 var user = new User();
+console.log(user);
+//------------------------//
+// 76.Constructor Function
+//------------------------//
+var User1 = /** @class */ (function () {
+    function User1(name, email) {
+        this.name = "Jhon";
+        this.email = "jj@gmail.com";
+    }
+    User1.prototype.greet = function () {
+        return "hello jhon";
+    };
+    return User1;
+}());
+var user1 = new User();
+var user2 = new User1("JJ", "joyng808@gmail.com");
 console.log(user);
--- index.ts
+++ index.ts
@@ -20,6 +20,21 @@ console.log(user);
 // 76.Constructor Function
 //------------------------//
 
+class User1{
+    name = "Jhon";
+    email = "jj@gmail.com";
+
+    constructor(name:string, email:string){}
+
+    greet(){
+        return `hello jhon`;
+    }
+}
+
+const user1 = new User();
+const user2 = new User1("JJ","joyng808@gmail.com");
+
+console.log(user);
 
 
 
