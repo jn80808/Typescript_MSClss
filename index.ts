83b8cad3f11eb2c298c3671e785e0381e65f3983
JN <joyng808@gmail.com>
2026-01-06 22:59:23 +0800

    [JN]-Update

--- index.ts
+++ index.ts
@@ -38,7 +38,32 @@ console.log(user);
 
 
 
+//------------------------//
+// 77.This keyword
+//------------------------//
 
+class User2{
+    //Before
+    // name = "Jhon";
+    // email = "jj@gmail.com";
+
+    name: string;
+    email: string;
+
+    constructor(name:string, email:string){
+        this.name = name;
+        console.log(email);
+    }
+
+    greet(){
+        return `hello jhon`;
+    }
+}
+
+const user3 = new User();
+const user4 = new User1("JJ","joyng808@gmail.com");
+
+console.log(user);
 
 
 
