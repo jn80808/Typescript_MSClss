9fbe3aea88394b5b4aea023312701b3f1e567f13
JN <joyng808@gmail.com>
2026-01-29 21:58:53 +0800

    [JN]-Update

--- index.ts
+++ index.ts
@@ -160,8 +160,6 @@ console.log(admin);
 //----------------------------//
 
 
-
-
 class Admin2 extends User3 {
     isAdmin: boolean = true;
     userReporting: number;
@@ -186,10 +184,68 @@ const admin3: Admin2 = new Admin2("Mark", "marker@gmail.com",11);
 console.log(user);
 console.log(admin);
 
+//----------------------------//
+// 82.Practice Classes  
+//----------------------------//
+
+/**
+ * Practice Problem 
+ * You are building a simple library management system.
+ * Implement the following requirements using Typescript:
+ * 
+ * 
+ *TODO: 1.Create a class Book with the following properties: 
+ * - title (string, required)
+ * - author (string, required)
+ * - yearPublished (number, optional)
+ * - ISBN (string, readonly)
+ * 
+ * TODO: 2.Define a construction function to initialize the Book class with 
+ * title, author, yearPublished, and ISBN 
+ * 
+ * TODO: 3.Ensure that the constructor function uses the this keyword to assign values to the properties 
+ * 
+ * TODO 4. Create an Instance of the Book Class and log its details 
+ * 
+ * TODO 5. Create a function logBookDetails that takes an instance of Book as a parameter and logs its details 
+ * 
+ * TODO 6. Cretae a subclass EBook that extends the Book class. Add the following properties:
+ * - fileSize(number, required)
+ * - format (string, required)
+ * 
+ * TODO: 7. Use the super method to call the constructor of the parent class Book from the EBook class.
+ * 
+ * TODO: 8. Ensure that the yearPubslished property in the Book Class is Optional and the ISBN property is readonly
+ */
+
+//----------------------------------//
+// 83.Practice Classes Solution 
+//----------------------------------//
+
+
+class Book {
+    title: string;
+    author:string;
+    yearPublish?: number;
+    readonly ISBN : string;
+
+
+    constructor( 
+        title: string, 
+        author: string, 
+        ISBN: string,
+        yearPublish?: number
+    ){
+        this.title = title;
+        this.ISBN = ISBN;
+        this.author = author;
+
+    }
 
 
 
 
+}
 
 
 
