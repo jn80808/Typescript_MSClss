f4ddcfeb35f87f5493ea4429dca2f3ad1e94f612
JN <joyng808@gmail.com>
2026-02-10 23:37:15 +0800

    [JN]- More Control Over Classes

---
+++ index2.ts
@@ -0,0 +1,16 @@
+//----------------------------------------------//
+// 91.More Control Over Classes 
+//---------------------------------------------//
+
+class Person {
+
+    constructor(    
+    public firstName: string,
+    public lastName: string,
+    public age: number
+) {}
+
+}
+
+const person1 = new Person("John", "Doe", 30);
+const person2 = new Person("Jane", "Smith", 25);
