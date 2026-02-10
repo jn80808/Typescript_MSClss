c14260825cac583c3d28f9a120b40db91dac8094
JN <joyng808@gmail.com>
2026-02-10 23:38:57 +0800

    [JN]- Adding validation

--- index2.ts
+++ index2.ts
@@ -8,9 +8,12 @@ class Person {
     public firstName: string,
     public lastName: string,
     public age: number
-) {}
-
+) {
+    if (age > 200 || age < 0) {
+        throw new Error("Age must be between 0 and 200");
+    }
+}
 }
 
-const person1 = new Person("John", "Doe", 30);
-const person2 = new Person("Jane", "Smith", 25);
+const person1 = new Person("John", "Doe", 300);
+const person2 = new Person("Jane", "Smith", -25);
