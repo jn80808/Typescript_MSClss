36bafa7b3645efbe3a895bd4d89f09191b46da07
JN <joyng808@gmail.com>
2026-03-04 21:16:05 +0800

    [JN]-Update

--- Abstrac Classes And Interfaces.ts
+++ Abstrac Classes And Interfaces.ts
@@ -2,5 +2,21 @@
 // 104. Introduction to Abstract Classes 
 //---------------------------------------------//
 
+class Department {
+    constructor(public name: string) {
+    }
+}
+
+abstract class Department2 {
+    constructor(public name1: string) {
+    }
+}
+
+
+
+
+
+
+
 
 
