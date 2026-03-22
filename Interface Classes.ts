99c457d8a938bd066a46ec56e8f54f3c2b1eee60
JN <joyng808@gmail.com>
2026-03-22 22:36:45 +0800

    [JN]-Update

--- Interface Classes.ts
+++ Interface Classes.ts
@@ -1,4 +1,4 @@
-interface User {
+interface User1 {
     username : string;
     email : string;
     login() : void;
@@ -6,7 +6,7 @@ interface User {
 }
 
 
-class Admin implements User {
+class Admin1 implements User1 {
     constructor (
         public username : string, 
         public email : string, 
@@ -18,7 +18,7 @@ class Admin implements User {
 
 }
 
-class Customer implements User {
+class Customer1 implements User1 {
     constructor (
         public username : string,
         public email : string,
@@ -33,16 +33,141 @@ class Customer implements User {
 
 
 class Auth {
-    public static login(user : User) : void {
+    public static login(user : User1) : void {
         user.login();
     }
 }
 
 
-const admin: Admin = new Admin("joy", "joy@gmail.com", 1);
-const customer: Customer = new Customer("mike", "mike@gmail.com", "laptop, phone", ["laptop", "phone"]);
+const admin: Admin1 = new Admin1("joy", "joy@gmail.com", 1);
+const customer: Customer1 = new Customer1("mike", "mike@gmail.com", "laptop, phone", ["laptop", "phone"]);
 
 Auth.login(admin);
 Auth.login(customer);
+
+
+//--------------------------------------------------//
+// 112. Introduction to Interfaces in TypeScript
+//-------------------------------------------------//
+
+interface Person {
+    name : string;
+    email : string;
+    age: number;
+    phone?: number; // Optional property
+    greet?: () => void; // Optional method
+
+}
+
+const person : Person = {
+    name : "joy",
+    email: "joy@gmail.com",
+    age: 27
+}
+
+//--------------------------------------------------//
+// 113. Extending Interfaces in TypeScript
+//-------------------------------------------------//
+
+interface User {
+    name: string;
+    email: string;
+    phone: number;
+    gender: string;
+}
+
+interface UserWithAddres extends User {
+    Address: string;
+}
+
+
+const useA: User={
+    name: "joy",
+    email: "joy@gmail.com",
+    phone: 1234567890,
+    gender: "female"
+}
+
+
+
+const userWithAddress: UserWithAddres = {
+    name: "joy",
+    email: "joy@gmail.com",
+    phone: 1234567890,
+    gender: "female",
+    Address:"123 Main St",
+}
+
+//--------------------------------------------------//
+// 114. Inheriting from Multiple Interfaces in TypeScript
+//-------------------------------------------------//
+
+enum Roles {
+    admin = "admin",
+    author = "author",
+    editor = "editor",
+}
+
+interface Role {
+    role: Roles;
+}
+
+enum PermissionsList {
+    read = "read",
+    write = "write",
+    execute = "execute",
+}
+
+interface Permission {
+    permission: PermissionsList[];
+
+}
+
+interface UserWithAddresB extends UserB {
+    Address: string;
+}
+
+interface UserB {
+    name: string;
+    email: string;
+    phone: number;
+    gender: string;
+}
+
+
+interface AdminUser extends UserB, Role, Permission {
+    numberofUsersReporting: number;
+
+}
+
+
+const useB: UserB={
+    name: "joy",
+    email: "joy@gmail.com",
+    phone: 1234567890,
+    gender: "female"
+}
+
+
+
+const userWithAddressB: UserWithAddresB = {
+    name: "joy",
+    email: "joy@gmail.com",
+    phone: 1234567890,
+    gender: "female",
+    Address:"123 Main St",
+}
+
+const rob : AdminUser = {
+    name: "rob",
+    email: "rob@gmail.com",
+    phone: 9876543210,
+    gender: "male",
+    role: Roles.admin,
+    permission: [PermissionsList.read, PermissionsList.write],
+    numberofUsersReporting: 5,
+}
+
+
 
 
