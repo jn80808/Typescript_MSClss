06579f97f9c67bb6f616975973fa6635687b9a65
JN <joyng808@gmail.com>
2026-03-25 22:37:22 +0800

    [JN]-117.Using Multiple Generics in Interfaces in TypeScript

--- Interface Classes.ts
+++ Interface Classes.ts
@@ -173,7 +173,7 @@ const rob : AdminUser = {
 //-------------------------------------------------//
 
 
-interface Automobile {
+interface Automobiles1 {
     type: string;
     brand: string;
     colors: string[];
@@ -183,12 +183,124 @@ interface Automobile {
 
 
 
+//type - car
+//brand - toyota, ferrari, honda
+//colors - red, blue, white
+//description - a reliable and fuel-efficient car
 
 
+//--------------------------------------------------//
+// 116. Assigning Generic to Interfaces in TypeScript
+//-------------------------------------------------//
+
+enum AutoMobileType {
+    car = "car",
+    truck = "truck",
+    motorcycle = "motorcycle",
+    bus = "bus",
+    bike = "bike",
+    van = "van",
+}
+
+enum AutoMobileBrand {
+    toyota = "toyota",
+    ferrari = "ferrari",
+    honda = "honda",
+    ford = "ford",
+    chevrolet = "chevrolet",
+    bmw = "bmw",
+}
+
+enum AutoMobileColors {
+    red = "red",
+    blue = "blue",
+    white = "white",
+    black = "black",
+    silver = "silver",
+    gray = "gray",
+}
 
 
+interface Automobile<Type, Brand, Colors> {
+    type: Type;
+    brand: Brand;
+    colors: Colors[];
+    description: string;
+    
+}
 
 
+//--------------------------------------------------------//
+// 117.Using Multiple Generics in Interfaces in TypeScript
+//--------------------------------------------------------//
+
+enum AutoMobileType1 {
+    car = "car",
+    truck = "truck",
+    motorcycle = "motorcycle",
+    bus = "bus",
+    bike = "bike",
+    van = "van",
+}
+
+enum AutoMobileBrand1 {
+    toyota = "toyota",
+    ferrari = "ferrari",
+    honda = "honda",
+    ford = "ford",
+    chevrolet = "chevrolet",
+    bmw = "bmw",
+}
+
+enum AutoMobileColors1 {
+    red = "red",
+    blue = "blue",
+    white = "white",
+    black = "black",
+    silver = "silver",
+    gray = "gray",
+}
+
+
+interface Automobile1<Type, Brand, Colors> {
+    type: Type;
+    brand: Brand;
+    colors: Colors[];
+    description: string;
+    
+}
+
+// Example of multiple generics in interfaces
+const ferrari: Automobile1<AutoMobileType1, AutoMobileBrand1, AutoMobileColors1> = {
+    type: AutoMobileType1.car,
+    brand: AutoMobileBrand1.ferrari,
+    colors: [AutoMobileColors1.red, AutoMobileColors1.black],
+    description: "A high-performance sports car known for its speed and luxury."
+}
+
+// another example of honda with its type, brand and colors as strings
+const honda : Automobile1<string, string, string> = {
+    type: "car",
+    brand: "honda",
+    colors: ["red", "blue"],
+    description: "A reliable and fuel-efficient car."
+}
+
+// another example that if toyota for instance might have a number of codes for its colors, where is not a strings 
+// we create a toyota automobile with its colours as numbers 
+
+
+const toyota : Automobile1<string, AutoMobileBrand1, number> = {
+    type: "car",
+    brand: AutoMobileBrand1.toyota,
+    colors: [1, 2, 3], // Assuming these numbers represent specific color codes for Toyota
+    description: "A reliable and fuel-efficient car."
+}
+
+// console log all of this 3 example 
+console.log(ferrari);
+console.log(honda);
+console.log(toyota);
 
 
 
