a3a57067f391f4f454cf3f76be7a477666261cb9
JN <joyng808@gmail.com>
2026-04-13 21:55:07 +0800

    [JN]-128. Module Introduction - The TypeScript Compiler

---
+++ TypescriptCompiler.ts
@@ -0,0 +1,20 @@
+//--------------------------------------------------------------//
+// 128. Module Introduction - The TypeScript Compiler 
+//-------------------------------------------------------------//
+
+
+//1. File Options : Options related to how TypeScript treats TypeScript Files.
+//2. Type Checking Options : Options that alter TypeScript's behaivior related to type checking and other code related and other code related features that typeScript offeres.
+//                           Then we look at various type checking options that are available to us in Tsconfig file.
+//                           These options impact the behavior of TypeScript when it comes to type checking, and various other features that are offered by TypeScript.
+//3.File Watcher and Source Maps : We look at how we can use the built in file watcher and source maps during development with typescript.
+//We will also create npm scripts to automate our development workflow.
+
+
+
+
+
+
+
+
+
