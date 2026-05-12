fcb64af3542c0be10319207aea23f9c99b037f91
JN <joyng808@gmail.com>
2026-05-12 17:45:09 +0800

    [JN]- referesh

---
+++ src/OutFiles/General.ts
@@ -0,0 +1,51 @@
+//duck
+
+//Declaration and Annotation in TypeScript
+
+//declaration: is declaring a type 
+
+type CustomString = string;
+
+type CustomNumber = number;
+
+type User = {
+    username : string;
+    email : string;
+    login() : void;
+}
+
+type CustomDate = Date;
+
+type CustomArray<T> = T[];
+
+type CustomSymbol = symbol;
+
+//annotation : when you assigning a type to a variable or function parameter or return type
+
+let firstName : CustomString = "John";
+let ageJohn : CustomNumber = 30;
+let userJohn: User = {
+    username : "john_doe",
+    email : "john.doe@example.com"
+}
+
+let today : CustomDate = new Date();
+let numbers : CustomArray<number> = [1, 2, 3, 4, 5];
+
+function greetUser(user : User) : void {
+    console.log(`Hello, ${user.username}!`);
+}
+greetUser(userJohn);
+
+let uniqueId : CustomSymbol = Symbol("id");
+
+let unique : CustomSymbol = Symbol();
+
+
+function addNumbers(a : CustomNumber, b : CustomNumber) : CustomNumber {
+    return a + b;
+}   
+
+let sum : CustomNumber = addNumbers(5, 10);
+console.log(sum);
+
