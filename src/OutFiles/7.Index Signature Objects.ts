596e759e9b70762c9a71099ed892bc7907ebdd6a
JN <joyng808@gmail.com>
2026-05-19 22:23:38 +0800

    [JN]- Signature Objects

---
+++ src/OutFiles/7.Index Signature Objects.ts
@@ -0,0 +1,82 @@
+// so here we will learn how we will strictly our nested object 
+type Post = {
+    title: string ;
+    description: string;
+    content: string;
+    date: Date;
+    author: { //nested object type
+        name: string;
+        email: string;
+    },
+    awards:{
+        [key: string]: { //index signature for awards object
+            name: string;
+            year: number;
+        }
+    };
+};
+
+type Award = {  
+    [key: string]: { //index signature for awards object    ---- from this 
+        name: string;
+        year: number;
+    }
+}
+
+type AwardsDetails = {
+    name: string;
+    year: number;
+}
+
+type Awards = {
+    [key: string]: AwardsDetails; //index signature for awards object --- to this 
+}
+
+
+
+
+//ex.1 
+let post: Post ={
+    title: "Type Alias for objects",
+    description: "Type Alias for objects in TypeScript",
+    content:"Type aliases provide a way to create a new name for an existing type. This can make your code more readable and maintainable.",
+    date: new Date(),
+    author: { //this is nested object and we need to define its type as well in our Post type alias
+        name: "John Doe",
+        email: "john.doe@example.com"
+    },
+    awards1:{
+    name: "Best Post of the Year",
+    year: 2023
+    },
+    awards2:{
+    name: "Best Author of the Year",
+    year: 2023
+    }
+}
+
+
+//ex.2
+let post2: Post ={
+    title: "Type Alias for objects",
+    description: "Type Alias for objects in TypeScript",
+    content:"Type aliases provide a way to create a new name for an existing type. This can make your code more readable and maintainable.",
+    date: new Date(),
+    author: {
+        name: "John Doe",
+        email: "john.doe@example.com"
+    },
+    awards: {
+        awards1: {
+            name: "Best Post of the Year",
+            year: 2023
+        },
+        awards2: {
+            name: "Best Author of the Year",
+            year: 2023
+        }
+    }
+};
+
+
+
