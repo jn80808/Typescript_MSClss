e09af9da007f1cd4b9f1d29910b7826cb9b33593
JN <joyng808@gmail.com>
2026-05-28 23:10:45 +0800

    [JN]-Function

--- FunctionInTypeScript/5.Async Functions.ts
+++ FunctionInTypeScript/5.Async Functions.ts
@@ -1,3 +1,9 @@
+// let's talk about asynchronous functions and how you can strictly type async functions
+// in TypeScript.
+// Asynchronous functions in JavaScript always return a promise.
+// We use the async keyword when defining asynchronous functions in JavaScript.
+
+
 
 //-------having Promise string ----------//
 async function fethFromDatabase(id: number) : Promise<any> {}
