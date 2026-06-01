9ef9723d1c1599d39d558cafe86da1c624338732
JN <joyng808@gmail.com>
2026-06-01 20:11:23 +0800

    [JN]- Generic Functions

--- GenericTypeTypeScript/1.What are Generics.ts
+++ GenericTypeTypeScript/1.What are Generics.ts
@@ -1,3 +1,90 @@
+// let us assume that we have a function called return params.
+// This particular function has one parameter that is the param.
+
+function returnParams (param){
+    return param;
+}
+
+//Right now this function can take in any parameter and return that from within this function.
+
+
+
+function returnParams1 (param: any){
+    return param;
+}
+
+
+// we have generics.
+// Consider generics like variables which are declared before the function declaration, so that TypeScript
+// knows that this particular type would be used across the entire function.
+// we can define generic by using angle bracket <>
+
+
+// Between these angle brackets I declare a name for the variable or name for the generic, and I call it "Type".
+// This type is the type of value with which this function would be invoked
+
+
+function returnParams2<Type> (param: Type){
+    return param;
+}
+
+// So what we are saying over here is with whatever type this function is invoked, is the type of the
+// parameter.
+// It could be invoked with a string.
+// Then the parameter is of string type
+// It could be invoked with a number.
+// So the parameter is of number type.
+
+//so the <Type> the type inside of the angle braket is acts as variable 
+
+
+// In the end, the function will always return the same type.
+
+function returnParams3<Type> (param: Type): Type { 
+    return param;
+}
+
+// That's how this function is working.
+// returnParams3 is now is a complete generic function.
+
+
+// so now lets test it 
+returnParams3<String>("String"); // so here if the <String> the value should be string 
+returnParams3<String>(12222);   // if the type we put is string and the value we put is number its gets an error 
+
+//so her we can use string as type 
+const return1 = returnParams3<String>("12222"); 
+console.log(return1);
+
+//we can use number to 
+
+const return2 = returnParams3<Number>(11); 
+console.log(return2);
+
+
+//Generic Type of arrow function () =>
+const myParam: <T>(param:T ) => T = (param) => param;
+
+//Generic Type of function expression 
+const myparam2 = function <U> (param: U): U { 
+    return param;
+}
+
+
+//Generic type in  object 
+
+type ObjectType = {
+    myParam:<V>(param: V )=> V ,
+    myParam1:<V, X>(param1: V, param2: X ) => V|X //and the return V|X either V or X 
+}
+
+
+
+//Generic Type with Type Alias 
+type MyParam = <K>(param: K) => K;
+
+
+
 
 
 
---
+++ GenericTypeTypeScript/2.Generic Function Declarations.ts
@@ -0,0 +1,86 @@
+type GetFirstElement = <T>(arr: T[]) => T | undefined;
+
+
+
+const getFirstElement: GetFirstElement = (arr)=>{
+    return arr[0];
+}
+
+
+const numberArray = [1,2,3,4,5];
+const stringArray = ["a", "b", "c", "d", "e"];
+
+console.log(getFirstElement(numberArray)); // Output: 1
+console.log(getFirstElement(stringArray)); // Output: "a"
+
+let numberOutput  = getFirstElement<number>(numberArray); // TypeScript infers numberOutput as number 
+
+let numberOutput1  = getFirstElement(numberArray); // TypeScript infers numberOutput as number or undefined because the function can return undefined if the array is empty
+
+let stringOutput  = getFirstElement<string>(stringArray); // TypeScript infers stringOutput as string
+
+let stringOutput1  = getFirstElement(stringArray); // TypeScript infers stringOutput as string or undefined because the function can return undefined if the array is empty
+
+
+//------------------- Example: The Generic Function is in inside ------------------
+
+//so now this is different from above 
+//A
+type FirstElement <T> = (arr: T[]) => T | undefined;
+
+//VS 
+// B
+type GetirstElement = <T>(arr: T[]) => T | undefined;
+
+//so here the type of the function is different from above because here we are using the generic type in the type alias 
+// and in above we are using the generic type in the function declaration
+
+
+//@A : Generic on the type alias itself <T>  belongs to the type alias.
+//Think of it as: 
+type FirstElement1<number1> = (arr: number1[]) => number1 | undefined;
+type FirstElement2<string2> = (arr: string2[]) => string2 | undefined;
+
+// Type Alias
+//     │
+//     └── T
+//          │
+//          └── Function
+
+type FirstElement3<T> = (arr: T[]) => T | undefined;
+
+const getNumber: FirstElement3<number> =
+    (arr) => arr[0];
+
+getNumber([1, 2, 3]); // OK
+getNumber(["a"]);     // Error
+
+//@A : its only work on the number you specify <> at line 52 
+
+
+//@B : Generic on the function declaration <T> belongs to the function declaration.
+//the type alias itself is not generic Instead, it describes a generic function.
+//Think of it as: 
+type GetirstElement1 = <number1>(arr: number1[]) => number1 | undefined;
+type GetirstElement2 = <string2>(arr: string2[]) => string2 | undefined;
+
+
+// Type Alias
+//     │
+//     └── Generic Function
+//             │
+//             └── T
+
+
+type GetFirstElement1 = <T>(arr: T[]) => T | undefined;
+
+const getFirst: GetFirstElement1 =
+    (arr) => arr[0];
+
+getFirst([1, 2, 3]);      // number
+getFirst(["a", "b"]);     // string
+getFirst([true]);         // boolean
+
+//@B : its work on any type you specify <> at line 72  because the generic type is on the function declaration 
+// not on the type alias itself.
+
