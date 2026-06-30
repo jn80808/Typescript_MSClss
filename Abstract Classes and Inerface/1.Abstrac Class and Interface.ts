16ff1685faf53471c6e946de3c443cb9ce434e9c
Joy <joyng808@gmail.com>
2026-06-30 22:59:51 +0800

    [JN]- Abstract class

---
+++ Abstract Classes and Inerface/1.Abstrac Class and Interface.ts
@@ -0,0 +1,21 @@
+// static properties - What are static properties of a class along with real world
+// examples of static properties you already use in JavaScript.
+
+
+// abstract classes - Everything about Abstract classes and how are they different
+// from normal classes along with examples of inheritance and
+// their use with child classes.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
