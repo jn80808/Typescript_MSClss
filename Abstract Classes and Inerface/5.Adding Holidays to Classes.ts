d87d135aa80fc5cd0f7a28d370ddc3944a14b331
Joy <joyng808@gmail.com>
2026-07-06 23:09:35 +0800

    [JN]- Update

--- Abstract Classes and Inerface/5.Adding Holidays to Classes.ts
+++ Abstract Classes and Inerface/5.Adding Holidays to Classes.ts
@@ -44,8 +44,60 @@ class AdminDepartment extends Department {
 
     }
 
+//--create constants for holidays for ITDepartment and AdminDepartment
+const itHolidays: Holiday = [
+    {
+        date: new Date('2024-01-01'),
+        reason: 'IT Day'
+    },
+
+    {
+        date: new Date('2024-01-01'),
+        reason: 'New Year\'s Day'
+    }
+
+
+]; 
+
+const adminHolidays: Holiday = [
+
+    {
+        date: new Date('2024-01-01'),
+        reason: 'Admins Day'
+    },
+    {
+        date: new Date('2024-12-25'),
+        reason: 'Christmas Day'
+    }
+
+
+]; 
+
+
+const itDepartment: ITDepartment = new ITDepartment();
+itDepartment.addHolidays(itHolidays);
+
+const adminDepartment: AdminDepartment = new AdminDepartment();
+adminDepartment.addHolidays(adminHolidays);
 
 
 
+//-- add holidays to ITDepartment and AdminDepartment using addHolidays method
+itDepartment.addHolidays([
+    {
+        date: new Date('2024-07-04'),
+        reason: 'Independence Day'
+    }
+]);
 
-    
+
+adminDepartment.addHolidays([
+    {
+        date: new Date('2024-11-28'),
+        reason: 'Thanksgiving Day'
+    }
+]);
+
+console.log('IT Department Holidays:', itDepartment);
+console.log('Admin Department Holidays:', adminDepartment);
+
