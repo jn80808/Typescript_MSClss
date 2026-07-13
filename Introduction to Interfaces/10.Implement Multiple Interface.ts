5a150ca3deed9e19de99c8e687e86180e2330930
Joy <joyng808@gmail.com>
2026-07-13 23:50:26 +0800

    [JN]-Implement Multiple Interface

---
+++ Introduction to Interfaces/10.Implement Multiple Interface.ts
@@ -0,0 +1,74 @@
+
+enum AutomobileTypes {
+    car = "car",
+    bus = "bus",
+    van = "van",
+    truck ="truck",
+    bike = "bike",
+}
+
+
+
+enum AutomobileBrands {
+    ferrari = "ferrari",
+    honda = "honda",
+    bmw ="bmw",
+    toyota = "toyota",
+
+}
+
+
+enum AutomobileColors {
+    red = "red",
+    blue = "blue",
+    white = "white",
+    black = "black",
+    silver = "silver",
+}
+
+
+//Generic : <Type, Brand, Colors>
+interface Automobile<Type, Brand, Colors> {
+    type: Type;
+    brand: Brand;
+    colors: Colors[];
+    description: string;
+
+}
+
+
+//EX.1 Create Truck Car that Implement Interface 
+class Truck implements Automobile<string, AutomobileBrands, AutomobileColors> {
+    public type: string = "truck";
+
+    constructor(
+        public brand: AutomobileBrands,
+        public colors: AutomobileColors[],
+        public description: string
+    ) {}
+}
+
+
+//Create an intance of Truck Class 
+
+const toyotaTruck: Truck = new Truck(
+    AutomobileBrands.toyota,
+    [AutomobileColors.black, AutomobileColors.silver],
+    "This is a Toyota Truck"
+);
+
+
+console.log(toyotaTruck);
+
+// OUTPUT :
+// Truck {
+//   type: "truck",
+//   brand: "toyota",
+//   colors: ["black", "silver"],
+//   description: "This is a Toyota Truck"
+// }
+
+
+
+
+
