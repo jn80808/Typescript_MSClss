39473d0651d4ff4165e7dc9180be231133f6a5d1
Joy <joyng808@gmail.com>
2026-07-23 23:25:18 +0800

    [JN]- Interface and Access

--- Introduction to Interfaces/11.Multiple Inheritance in classes using interface.ts
+++ Introduction to Interfaces/11.Multiple Inheritance in classes using interface.ts
@@ -10,3 +10,17 @@ class Password {
 
 
 class RegisteredUser extends User, Password {}
+
+// This is what I said previously as well.
+
+// You can only inherit a single class in TypeScript.
+
+// You cannot inherit from multiple classes.
+
+// This problem is, however, solved by interfaces.
+
+// If you have a look at our truck class over here.
+
+// Again, in TypeScript, you can inherit from only one parent class, but classes can implement as many
+
+// interfaces as it wants to, and there is no limitation on that.
